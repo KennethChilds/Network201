7a4db57b8ad84ae3fc9c30f8db4b628f7a96137c
Kenneth <kenneth@kenneth.biz>
2023-12-24 18:50:17 -0500

    wip colorscheme and placement (text wise)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,25 +5,25 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>harvest</title>
-        <meta name="harvest is a service that allows you to realize losses on unsellable nft's"/>
+        <title>Network201</title>
+        <meta name="Network201 Is a Network of Young and Talented People that are Interested in Building the Future"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="">
         <div className="flex h-screen flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <span className="">harvest</span>
+            <span className="">Network201</span>
           </h1>
           <h2 className="text-4xl container flex flex-col items-center justify-center px-4">
-            <span className="">realize losses on unsellable nft's!</span>
+            <span className="">A Network Of Exceptional, Young Talent</span>
           </h2>
           <div className="gap-4">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-violet-500 p-4 text-white hover:bg-violet-600/100"
-              href=""
+              href="https://forms.gle/1MPUxMucvZwvKP5n8"
               target="_blank"
             >
-              <h3 className="text-2xl font-bold">Connect Your Wallet →</h3>
+              <h3 className="text-2xl font-bold">Apply →</h3>
             </Link>
           </div>
         </div>
